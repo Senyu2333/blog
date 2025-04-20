3d37c7b09a5a0da20e3ffd36fb8ee2e8f47d540a
森雨Senyu <1243669851@qq.com>
2025-04-20 20:05:46 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/blog/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "森雨阁",
+  description: "森雨的博客",
 
   theme,
 
