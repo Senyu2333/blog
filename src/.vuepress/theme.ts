4c5e81d602812896183ea99ed584bfc5329c9518
森雨Senyu <1243669851@qq.com>
2025-04-21 19:42:00 +0800

    Update theme.ts

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -11,7 +11,7 @@ export default hopeTheme({
     url: "https://mister-hope.com",
   },
 
-  logo: "https://theme-hope-assets.vuejs.press/logo.svg",
+  logo: "https://senyu2333.github.io/blog/src/vuepress/public/logo.png",
 
   repo: "vuepress-theme-hope/vuepress-theme-hope",
 
